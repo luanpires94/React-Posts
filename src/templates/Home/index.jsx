854bf21a6cd96902efc3516ae78f3ab0af88f113
luanpires94 <luan.94pires@gmail.com>
2021-04-17 20:17:24 -0300

    updating

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -33,7 +33,6 @@ const Home = () => {
   }, [])
 
   useEffect(() => {
-    console.log(new Date().toLocaleString('pt-br'))
     handleLoadPosts(0, postsPerPage)
   }, [handleLoadPosts, postsPerPage])
 
